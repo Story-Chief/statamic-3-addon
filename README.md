c9215db001bbb9bd555cab396b3bc52ce3411fcc
gregory <gregory@storychief.io>
2020-10-29 10:37:03 +0100

    Change endpoint URL to '/!/StoryChief/webhook' to mimic v2

--- README.md
+++ README.md
@@ -49,7 +49,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/!/storychief/webhook'
+        '/!/StoryChief/webhook'
     ];
 }
 ```
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use StoryChief\StoryChief\Controllers\WebhookController as StoryChiefWebhookController;
 use StoryChief\StoryChief\Middleware\HmacCheckMiddleware as StoryChiefHmacCheckMiddleware;
 
-Route::post(config('statamic.routes.action') . '/storychief/webhook',
+Route::post(config('statamic.routes.action') . '/StoryChief/webhook',
   [StoryChiefWebhookController::class, 'handle'])
   ->middleware([
     StoryChiefHmacCheckMiddleware::class,
